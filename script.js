ccaf9843c7f942ea3daa9a1a629a55f726cf2165
Shreyas sathe <shrishreyash@gmail.com>
2024-04-09 22:25:51 +0530

    date update

--- script.js
+++ script.js
@@ -10,14 +10,29 @@ const OutPutDate = document.querySelector('.date');
 
 const btn = document.querySelector('.btn')
 
+// 5 t0 10
+const min = 5;
+const max = 10;
+
+const randomDate = Math.floor(Math.random() * (10 -5  + 1)) + 5;
+
+const date = new Date(`2024, ${randomDate}`);
 //now i have taken all the outputs
 
 btn.addEventListener('click', DisplayOut);
 
+
+
+
 function DisplayOut(){
 
      OutPutCode.innerHTML = ProdCode.value;
-    OutPutDate.innerHTML = DateId.value;
+     DateId.value = date;
     console.log("Working date")
+    
+
+  
 }
+
+console.log(date);
 
