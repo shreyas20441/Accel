1dbb8e3ef34d136930c63fa4afa737ec506d6691
Shreyas sathe <shrishreyash@gmail.com>
2024-04-10 15:09:50 +0530

    Code and Date update

--- script.js
+++ script.js
@@ -10,20 +10,32 @@ const OutPutDate = document.querySelector('.Date');
 
 const btn = document.querySelector('.btn')
 
-// 5 t0 10
-const min = 5;
-const max = 10;
 
-const randomDate = Math.floor(Math.random() * (10 -5  + 1)) + 5;
 
-const date = new Date(`2024, ${randomDate}`);
+
+
+let RanDate;
+
+let date;
+
 //now i have taken all the outputs
 
 btn.addEventListener('click', ()=>{
 
+   
+
+    if(ProdCode.value != ""){
+        RanDate = Math.floor(Math.random() * (10 -5  + 1)) + 5;
+        date = new Date(`2024, ${RanDate}`);
+    }
+
+  
+
+    ProdCode.value = "";
+  
     OutPutCode.innerHTML = ProdCode.value;
     DateId.value = date;
-    OutPutDate.innerHTML = date;
+   
 });
 
 
